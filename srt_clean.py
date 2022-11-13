cc53d359bddd17bbde61be69180d2b8074c90b08
cabe74263 <shane.a.black@gmail.com>
2022-11-13 17:04:49 -0500

    1) add basic logging

--- srt_clean.py
+++ srt_clean.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
-
+import logging
 from helpers import *
 
+logging.basicConfig(filename='error.log', filemode='w',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 current_directory = os.getcwd()
 # current_directory = "C:\\media\\temp"
@@ -14,14 +16,15 @@ for item in files:
 
 print("")
 
-
 for file in files:
-    original_subs = read_file_to_list(file)
-    cleaned_subs = remove_ads(original_subs)
+    try:
+        original_subs = read_file_to_list(file)
+        cleaned_subs = remove_ads(original_subs)
 
-    if not original_subs == cleaned_subs:
-        if ".srt" in file:
-            p = Path(file)
-            p.rename(p.with_suffix(".original"))
-            write_file(cleaned_subs, file)
-
+        if not original_subs == cleaned_subs:
+            if ".srt" in file:
+                p = Path(file)
+                p.rename(p.with_suffix(".original"))
+                write_file(cleaned_subs, file)
+    except Exception as e:
+        logging.error(f"Ran into an issue trying to deal with '{file}'! It was \n", exec_info=True)
